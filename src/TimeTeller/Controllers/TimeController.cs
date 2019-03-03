56bcd5fb0c110783fb9a2e1ff80ffa1b360496ef
Farzan Hajian <fh791@yahoo.com>
2019-03-03 23:22:48 +0330

    Adding API & NLog to file

---
+++ src/TimeTeller/Controllers/TimeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace TimeTeller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimeController : ControllerBase
+    {
+        private readonly ILogger logger;
+
+        public TimeController(ILoggerFactory loggerFactory)
+        {
+            logger = loggerFactory.CreateLogger("TimeController");
+        }
+
+        [HttpGet("{timeZoneOffset}")]
+        public ActionResult<dynamic> Get(double timeZoneOffset)
+        {
+            dynamic result = new
+            {
+                Time = DateTime.UtcNow.AddHours(timeZoneOffset).ToLongTimeString(),
+                TimeZoneOffset = timeZoneOffset
+            };
+
+            string log = result.ToString();
+            logger.LogInformation(log);
+
+            return result;
+        }
+    }
+}
---
+++ src/TimeTeller/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using NLog.Web;
+using Microsoft.Extensions.Logging;
+
+namespace TimeTeller
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                //.ConfigureLogging(logging =>
+                //  {
+                //      logging.ClearProviders();
+                //      logging.SetMinimumLevel(LogLevel.Trace);
+                //  })
+                .UseNLog();
+    }
+}

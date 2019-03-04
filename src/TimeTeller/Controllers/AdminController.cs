dc55626a76704a72019b14cc0320c45f8eb14b57
Farzan Hajian <fh791@yahoo.com>
2019-03-04 21:04:32 +0330

    Making NLog use LiteDB

---
+++ src/TimeTeller/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace TimeTeller.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet("logs")]
+        public ActionResult<string> Logs()
+        {
+            StringBuilder result = new StringBuilder();
+
+            using (var db = new LiteDatabase("NLog.db"))
+            {
+                var col = db.GetCollection("DefaultLog");
+                foreach (BsonDocument logEntry in col.FindAll()) result.Append(logEntry.ToString());
+            }
+
+            return result.ToString();
+        }
+    }
+}
